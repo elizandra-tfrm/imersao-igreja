467904eb4b536f8f479851768e6b8844026f32ea
Isaque Daniel Barbosa <isaque.danielb@gmail.com>
2026-08-10 09:59:42 -0400

    Conectar o site a URL do Apps Script publicado

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Cole aqui a URL do deployment do Apps Script (termina em /exec).
 // Deploy > Manage deployments > copiar a URL do Web App.
-const WEBAPP_URL = 'COLE_AQUI_A_URL_DO_APPS_SCRIPT_TERMINADA_EM_/exec';
+const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwLoDRGB3xaJ5ryAPVtWujZsKMOamii3M7N4Iq9X5LcWaUrZRdxr2Sm9ei6mp6jzBh0-A/exec';
